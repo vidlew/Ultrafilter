d0734d094619d7c01c4603d29d4b1858428af31a
David <vidlew@outlook.com>
2018-04-25 14:20:04 -0300

    new file:   Ultrafilter.hs

---
+++ Ultrafilter.hs
@@ -0,0 +1,23 @@
+module Ultrafilter (Ultrafilter) where
+
+data Ultrafilter a = Ultra ((a -> Bool) -> Bool)
+
+instance Functor Ultrafilter where{
+  fmap f (Ultra u) = Ultra $ u.(.f)
+}
+
+instance Applicative Ultrafilter where{
+  pure = return
+; u <*> v = do f <- u
+               x <- v
+               return $ f x
+}
+
+instance Monad Ultrafilter where{
+  return = Ultra . (flip ($))
+; u >>= f = m $ f <$> u where m (Ultra v) = Ultra $ v.(\s (Ultra w) -> w s)
+}
+
+
+
+
