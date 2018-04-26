02fed96418963a59193c4f16371468fa9985626f
David <vidlew@outlook.com>
2018-04-26 04:41:02 -0300

    modified:   Ultrafilter.hs

--- Ultrafilter.hs
+++ Ultrafilter.hs
@@ -1,12 +1,18 @@
 --All ultrafilters that can be defined in Haskell are principal (of the form "return something"),
 --so this module is totally useless.
 
-module Ultrafilter (Ultrafilter) where
+module Ultrafilter
+            ( Ultrafilter
+            , measure
+            ) where
 
---For Ultra u to be an ultrafilter, it must have the following property:
+--For Ultra u to be an ultrafilter, u must have the following property:
 --  For every finite list [p_0, p_1, ... p_k] :: [a -> Bool] with the preperty that, for every x :: a,
 --  there is exactly one i in [0...k] such that p_i x, there is exactly one i in [0...k] such that u p_i.
 data Ultrafilter a = Ultra ((a -> Bool) -> Bool)
+
+measure :: Ultrafilter a -> (a -> Bool) -> Bool
+measure (Ultra u) s = u s
 
 instance Functor Ultrafilter where{
   fmap f (Ultra u) = Ultra $ u.(.f)
